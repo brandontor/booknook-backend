ee7749aecdc5aa22ab56f818db4643466bffda53
Brandon Tor <brandon.tor@hotmail.com>
2023-03-24 14:28:04 -0400

    working on exposing port env

--- main.go
+++ main.go
@@ -1,23 +1,13 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/brandontor/booknookbackendgo/configs"
 	"github.com/brandontor/booknookbackendgo/routes"
 	"github.com/gofiber/fiber/v2"
 )
-
-func getPort() string {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = ":3000"
-	} else {
-		port = ":" + port
-	}
-
-	return port
-}
 
 func main() {
 	app := fiber.New()
@@ -28,6 +18,12 @@ func main() {
 	//routes
 	routes.RouterSetup(app)
 
-	app.Listen(getPort())
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "3000"
+	}
+
+	log.Fatal(app.Listen("0.0.0.0:" + port))
 
 }
